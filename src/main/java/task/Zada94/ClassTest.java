fd9577169ee6b82628872c06ad8c2200414cde4e
Maciek Kacz <maciek.kacz@wp.pl>
2023-03-16 13:46:03 +0100

    upgrade task

--- src/main/java/task/Zada94/ClassTest.java
+++ src/main/java/task/Zada94/ClassTest.java
@@ -1,20 +1,33 @@
 package task.Zada94;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
 
 public class ClassTest {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        List<Integer> numbers = new LinkedList<>();
         int maxValue = 100;
         int sum = 0;
 
         while (maxValue >= sum ) {
             System.out.println("Poadj liczbe");
             int number = sc.nextInt();
+            numbers.add(number);
             sum = sum + number;
         }
-        System.out.println("NIe możesz podać wiekszej liczby");
+        if (sum % 2 == 0){
+            System.out.println(sum +  " Liczba parzysta");
+        }
+        else {
+            System.out.println(sum + " Liczna nie parzysta");
+        }
+        System.out.println("Podane liczby to ");
 
+        numbers.forEach( text -> System.out.print(text + ", "));
+
+        System.out.println("NIe możesz podać wiekszej liczby " + sum);
 
     }
 }
