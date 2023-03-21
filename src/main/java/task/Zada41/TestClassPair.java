cebca8d1fddcf9dd9f0d5064c93d379e14d13d8b
Maciek Kacz <maciek.kacz@wp.pl>
2023-03-21 19:23:01 +0100

    Typ generetic

---
+++ src/main/java/task/Zada41/TestClassPair.java
@@ -0,0 +1,9 @@
+package task.Zada41;
+
+public class TestClassPair {
+    public static void main(String[] args) {
+        Pair<String, Integer> par1 = new Pair<>("Krzesło",5);
+        Pair<Integer, Boolean> pair2 = new Pair<>(5, true);
+        Pair<Double,Integer> pair4 = new Pair<>(5000.00, 200);
+    }
+}
